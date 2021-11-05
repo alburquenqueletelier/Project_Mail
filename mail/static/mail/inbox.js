afdb5e88a2e80a8d3ea47efb4fc2827530290867
alburquenqueletelier <alburquenque.letelier@gmail.com>
2021-11-04 23:56:58 -0300

    Se agrego funcion enviar mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', sent_mail);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +31,17 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function sent_mail(event){
+  event.preventDefault();
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+    })
+  })
+  .then(response => load_mailbox('sent'));
 }
